bac28fb3b93d15c22c45f7b36f7a5d83064f1369
Kolen Cheung <christian.kolen@gmail.com>
2017-04-24 02:02:39 -0700

    make autopep8Aggressive

--- tail/timestream/ground_template_filter_array.py
+++ tail/timestream/ground_template_filter_array.py
@@ -1,86 +1,99 @@
 import numpy as np
 
-def simple_map_prepare(nch,npix):
-	signal_map = np.zeros((nch,npix))
-	hits_map = np.zeros((nch,npix),dtype=np.int32)
-	return signal_map,hits_map
+
+def simple_map_prepare(nch, npix):
+    signal_map = np.zeros((nch, npix))
+    hits_map = np.zeros((nch, npix), dtype=np.int32)
+    return signal_map, hits_map
 
 # simple_map_python
-def simple_map(signal_map,hits_map,pointing,mask,array):
-	nch,nt = array.shape
-	npix = signal_map.shape[1]
 
-	for i in range(nch):
-		for j in range(nt):
-			if mask[i,j]:
-				signal_map[i,pointing[j]] += array[i,j]
-				hits_map[i,pointing[j]] += 1
-	ok = hits_map > 0
-	signal_map[ok] /= hits_map[ok]
+
+def simple_map(signal_map, hits_map, pointing, mask, array):
+    nch, nt = array.shape
+    npix = signal_map.shape[1]
+
+    for i in range(nch):
+        for j in range(nt):
+            if mask[i, j]:
+                signal_map[i, pointing[j]] += array[i, j]
+                hits_map[i, pointing[j]] += 1
+    ok = hits_map > 0
+    signal_map[ok] /= hits_map[ok]
 
 # simple_scan_subtract_python
-def simple_scan_subtract(signal_map,pointing,array):
-	nch,nt = array.shape
-	npix = signal_map.shape[1]
-	for i in range(nch):
-		for j in range(nt):
-			array[i,j] -= signal_map[i,pointing[j]]
 
-def ground_template_filter_array(array,az,mask,pixel_size,groundmap=False,lr=False,filtmask=None):
-	'''
-	Remove ground template from array timestreams
 
-	If groundmap = True, then do the exact opposite, and remove component from timestream that isn't fixed with the ground
+def simple_scan_subtract(signal_map, pointing, array):
+    nch, nt = array.shape
+    npix = signal_map.shape[1]
+    for i in range(nch):
+        for j in range(nt):
+            array[i, j] -= signal_map[i, pointing[j]]
 
-	filtmask means to compute the filter template with that subset of the data, operation is applied to data specified by mask
-	
-	In largepatch filtmask refers to wafermask_chan_filt
-	'''
-	nch,nt = array.shape
-	minaz = np.min(az)
-	maxaz = np.max(az)
 
-	assert mask.shape == array.shape
-	assert az.size == nt
-	assert 3*pixel_size < (maxaz-minaz)
+def ground_template_filter_array(
+        array,
+        az,
+        mask,
+        pixel_size,
+        groundmap=False,
+        lr=False,
+        filtmask=None):
+    '''
+    Remove ground template from array timestreams
 
-	if filtmask is None:
-		filtmask = mask
+    If groundmap = True, then do the exact opposite, and remove component from timestream that isn't fixed with the ground
 
-	npix = int(np.round((maxaz-minaz)/pixel_size))
-	pixel_size = (maxaz - minaz) / npix
+    filtmask means to compute the filter template with that subset of the data, operation is applied to data specified by mask
 
-	# bin at npix is used as a junk pixel
-	signal_map,hits_map = simple_map_prepare(nch,npix+1)
+    In largepatch filtmask refers to wafermask_chan_filt
+    '''
+    nch, nt = array.shape
+    minaz = np.min(az)
+    maxaz = np.max(az)
 
-	pointing = np.int_(np.floor(npix*(az - minaz)/(maxaz-minaz)))
-	# One point with az = maxaz will end up one bin too far left
-	pointing[pointing==npix] = npix - 1
-	assert np.max(pointing) < npix
-	assert np.min(pointing) >= 0
+    assert mask.shape == array.shape
+    assert az.size == nt
+    assert 3 * pixel_size < (maxaz - minaz)
 
-	if groundmap:
-		array_in = array.copy()
-	
-	if lr:
-		vaz = np.gradient(az)
-		l = vaz >= 0
-		r = vaz < 0
-		lp = pointing.copy()
-		lp[~l] = npix
-		rp = pointing.copy()
-		rp[~r] = npix
-		simple_map(signal_map,hits_map,lp,filtmask,array)
-		signal_map[:,npix] = 0
-		simple_scan_subtract(signal_map,lp,array)
-		signal_map[:] = 0
-		hits_map[:] = 0
-		simple_map(signal_map,hits_map,rp,filtmask,array)
-		signal_map[:,npix] = 0
-		simple_scan_subtract(signal_map,rp,array)
-	else:
-		simple_map(signal_map,hits_map,pointing,filtmask,array)
-		simple_scan_subtract(signal_map,pointing,array)
+    if filtmask is None:
+        filtmask = mask
 
-	if groundmap:
-		array[:,:] = array_in - array
+    npix = int(np.round((maxaz - minaz) / pixel_size))
+    pixel_size = (maxaz - minaz) / npix
+
+    # bin at npix is used as a junk pixel
+    signal_map, hits_map = simple_map_prepare(nch, npix + 1)
+
+    pointing = np.int_(np.floor(npix * (az - minaz) / (maxaz - minaz)))
+    # One point with az = maxaz will end up one bin too far left
+    pointing[pointing == npix] = npix - 1
+    assert np.max(pointing) < npix
+    assert np.min(pointing) >= 0
+
+    if groundmap:
+        array_in = array.copy()
+
+    if lr:
+        vaz = np.gradient(az)
+        l = vaz >= 0
+        r = vaz < 0
+        lp = pointing.copy()
+        lp[~l] = npix
+        rp = pointing.copy()
+        rp[~r] = npix
+        simple_map(signal_map, hits_map, lp, filtmask, array)
+        signal_map[:, npix] = 0
+        simple_scan_subtract(signal_map, lp, array)
+        signal_map[:] = 0
+        hits_map[:] = 0
+        simple_map(signal_map, hits_map, rp, filtmask, array)
+        signal_map[:, npix] = 0
+        simple_scan_subtract(signal_map, rp, array)
+    else:
+        simple_map(signal_map, hits_map, pointing, filtmask, array)
+        simple_scan_subtract(signal_map, pointing, array)
+
+    if groundmap:
+        array[:, :] = array_in - array
--- tail/timestream/poly_filter_array.py
+++ tail/timestream/poly_filter_array.py
@@ -1,95 +1,114 @@
 import numpy as np
 
+
 class LegCache():
-	def __init__(self):
-		self.d = {}
-		pass
-	def prep_legendre(self,n,polyorder):
-		p = (n,polyorder)
-		if p not in self.d:
-			self.d[p] = prep_legendre(n,polyorder)
-		return self.d[p]
+    def __init__(self):
+        self.d = {}
+        pass
 
-def prep_legendre(n,polyorder):
-	'''make array of legendre's'''
-	legendres=np.empty([n,polyorder+1])
-	legendres[:,0]=np.ones(n)
-	if polyorder > 0:
-		legendres[:,1]=np.linspace(-1,1,n)
-	for i in range(polyorder-1):
-		l = i+1
-		legendres[:,l+1]=((2*l+1)*legendres[:,1]*legendres[:,l]-l*legendres[:,l-1])/(l+1)
+    def prep_legendre(self, n, polyorder):
+        p = (n, polyorder)
+        if p not in self.d:
+            self.d[p] = prep_legendre(n, polyorder)
+        return self.d[p]
 
-	q,r=np.linalg.qr(legendres)
-	rinv = np.linalg.inv(r)
-	qt=q.T.copy()
-	return legendres,rinv,qt
 
-def poly_filter_array(array,mask_remove,mask,scan_list,ibegin,polyorder,minfrac=.75):
-	""" writes over input array
-	"""
-	nold=-1
-	#do nothing
-	if polyorder < 0:
-		return array
-	#damn, work
-	nch=array.shape[0]
-	nt=array.shape[1]
-	ns = len(scan_list)
+def prep_legendre(n, polyorder):
+    '''make array of legendre's'''
+    legendres = np.empty([n, polyorder + 1])
+    legendres[:, 0] = np.ones(n)
+    if polyorder > 0:
+        legendres[:, 1] = np.linspace(-1, 1, n)
+    for i in range(polyorder - 1):
+        l = i + 1
+        legendres[:,
+                  l + 1] = ((2 * l + 1) * legendres[:,
+                                                    1] * legendres[:,
+                                                                   l] - l * legendres[:,
+                                                                                      l - 1]) / (l + 1)
 
-	coeff_out = np.zeros((nch,ns,polyorder+1))
+    q, r = np.linalg.qr(legendres)
+    rinv = np.linalg.inv(r)
+    qt = q.T.copy()
+    return legendres, rinv, qt
 
-	legcache = LegCache()
 
-	#remove mean
-	if polyorder == 0:
-		for s in range(len(scan_list)):
-			istart,n = scan_list[s]
-			start=istart-ibegin
+def poly_filter_array(
+        array,
+        mask_remove,
+        mask,
+        scan_list,
+        ibegin,
+        polyorder,
+        minfrac=.75):
+    """ writes over input array
+    """
+    nold = -1
+    # do nothing
+    if polyorder < 0:
+        return array
+    #damn, work
+    nch = array.shape[0]
+    nt = array.shape[1]
+    ns = len(scan_list)
+
+    coeff_out = np.zeros((nch, ns, polyorder + 1))
+
+    legcache = LegCache()
+
+    # remove mean
+    if polyorder == 0:
+        for s in range(len(scan_list)):
+            istart, n = scan_list[s]
+            start = istart - ibegin
 #			array[:,start:start+n] -= tile(np.mean(array[:,start:start+n]*mask[:,start:start+n],axis=1).reshape(nch,1),[1,n])
-			for i in range(nch):
-				if np.any(mask[i,start:start+n]):
-					mean = np.average(array[i,start:start+n],weights=mask[i,start:start+n])
-					array[i,start:start+n] -= mean
-					coeff_out[i,s,0] = mean
-	
-	#other cases		
-	if polyorder > 0:
-		for s in range(len(scan_list)):
-			istart,n = scan_list[s]
-			start=istart-ibegin
-			if n<=polyorder: ## otherwise cannot compute legendre polynomials
-                                for i in xrange(nch):
-                                        mask[i,start:start+n]=0 #flag it
-                                        mask_remove[i,start:start+n]=0 #remove this region from actual data as well
-                                        print 'Not enough points (%d) to build legendre of order (%d)'%(n,polyorder)
-                                continue
-			goodhits = np.sum(mask[:,start:start+n],axis=1)
-			if n != nold:
-				legendres,rinv,qt = legcache.prep_legendre(n,polyorder)
-				rinvqt = np.dot(rinv,qt)
-				nold = n
-			#handle no masked ones
-			
-			for i in xrange(nch):
-				if goodhits[i] != n:
-					continue #skip for now
-				#filter_slice_legendre_qr_nomask_precalc_inplace(array[i,start:start+n],legendres,rinvqt)
-				bolo = array[i,start:start+n]
-				coeff = np.dot(rinvqt,bolo)
-				coeff_out[i,s,:] = coeff
-				bolo -= np.dot(legendres,coeff)
+            for i in range(nch):
+                if np.any(mask[i, start:start + n]):
+                    mean = np.average(
+                        array[i, start:start + n], weights=mask[i, start:start + n])
+                    array[i, start:start + n] -= mean
+                    coeff_out[i, s, 0] = mean
+
+    # other cases
+    if polyorder > 0:
+        for s in range(len(scan_list)):
+            istart, n = scan_list[s]
+            start = istart - ibegin
+            if n <= polyorder:  # otherwise cannot compute legendre polynomials
+                for i in xrange(nch):
+                    mask[i, start:start + n] = 0  # flag it
+                    # remove this region from actual data as well
+                    mask_remove[i, start:start + n] = 0
+                    print 'Not enough points (%d) to build legendre of order (%d)' % (n, polyorder)
+                continue
+            goodhits = np.sum(mask[:, start:start + n], axis=1)
+            if n != nold:
+                legendres, rinv, qt = legcache.prep_legendre(n, polyorder)
+                rinvqt = np.dot(rinv, qt)
+                nold = n
+            # handle no masked ones
+
+            for i in xrange(nch):
+                if goodhits[i] != n:
+                    continue  # skip for now
+                # filter_slice_legendre_qr_nomask_precalc_inplace(array[i,start:start+n],legendres,rinvqt)
+                bolo = array[i, start:start + n]
+                coeff = np.dot(rinvqt, bolo)
+                coeff_out[i, s, :] = coeff
+                bolo -= np.dot(legendres, coeff)
 #				array[i,start:start+n] = filter_slice_legendre_qr_nomask_precalc(
 #					array[i,start:start+n], legendres,rinv,qt)
 
-			for i in xrange(nch):
-				if goodhits[i] == n:
-					continue #skip since dealt with above
-				if goodhits[i] < minfrac*n: #not enough points
-					mask[i,start:start+n]=0 #flag it
-					mask_remove[i,start:start+n]=0 #remove this region from actual data as well
-					continue
-				bolo,coeff = filter_slice_legendre_qr_mask_precalc(array[i,start:start+n],mask[i,start:start+n],legendres)
-				array[i,start:start+n] = bolo
-				coeff_out[i,s,:] = coeff
-	return coeff_out
+            for i in xrange(nch):
+                if goodhits[i] == n:
+                    continue  # skip since dealt with above
+                if goodhits[i] < minfrac * n:  # not enough points
+                    mask[i, start:start + n] = 0  # flag it
+                    # remove this region from actual data as well
+                    mask_remove[i, start:start + n] = 0
+                    continue
+                bolo, coeff = filter_slice_legendre_qr_mask_precalc(
+                    array[i, start:start + n], mask[i, start:start + n], legendres)
+                array[i, start:start + n] = bolo
+                coeff_out[i, s, :] = coeff
+    return coeff_out
