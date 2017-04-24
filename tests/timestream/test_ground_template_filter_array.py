92bdbde44c0bd39f74d128645f7c975f3e509dd1
Kolen Cheung <christian.kolen@gmail.com>
2017-04-24 02:20:12 -0700

    tests: improve tests to cover both python2 and python3 cases

--- tests/timestream/test_ground_template_filter_array.py
+++ tests/timestream/test_ground_template_filter_array.py
@@ -3,13 +3,23 @@
 from .context import ground_template_filter_array
 import pickle
 from tests.helper import assertIdenticalList
+import sys
+py2 = sys.version_info[0] == 2
 
 
 def test_ground_template_filter_array():
     with open('tests/timestream/ground_template_filter_array_input.pkl', 'rb') as f:
-        ground_template_filter_array_input = pickle.load(f)
+        if not py2:
+            ground_template_filter_array_input = pickle.load(
+                f, encoding='latin1')
+        else:
+            ground_template_filter_array_input = pickle.load(f)
     with open('tests/timestream/ground_template_filter_array_output.pkl', 'rb') as f:
-        ground_template_filter_array_output = pickle.load(f)
+        if not py2:
+            ground_template_filter_array_output = pickle.load(
+                f, encoding='latin1')
+        else:
+            ground_template_filter_array_output = pickle.load(f)
     ground_template_filter_array(*ground_template_filter_array_input)
     assertIdenticalList(
         ground_template_filter_array_input,
--- tests/timestream/test_poly_filter_array.py
+++ tests/timestream/test_poly_filter_array.py
@@ -3,12 +3,20 @@
 from .context import poly_filter_array
 import pickle
 from tests.helper import assertIdenticalList
+import sys
+py2 = sys.version_info[0] == 2
 
 
 def test_poly_filter_array():
     with open('tests/timestream/poly_filter_array_input.pkl', 'rb') as f:
-        poly_filter_array_input = pickle.load(f)
+        if not py2:
+            poly_filter_array_input = pickle.load(f, encoding='latin1')
+        else:
+            poly_filter_array_input = pickle.load(f)
     with open('tests/timestream/poly_filter_array_output.pkl', 'rb') as f:
-        poly_filter_array_output = pickle.load(f)
+        if not py2:
+            poly_filter_array_output = pickle.load(f, encoding='latin1')
+        else:
+            poly_filter_array_output = pickle.load(f)
     poly_filter_array(*poly_filter_array_input)
     assertIdenticalList(poly_filter_array_input, poly_filter_array_output)
