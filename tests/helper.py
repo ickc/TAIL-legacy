55264c2b6efd898cb74b9abc5a7b07a7b0066d00
Kolen Cheung <christian.kolen@gmail.com>
2017-04-24 02:02:08 -0700

    make autopep8Aggressive

--- tests/helper.py
+++ tests/helper.py
@@ -1,9 +1,10 @@
 import numpy as np
 from numpy.testing import assert_allclose
 
+
 def assertIdenticalList(list1, list2):
-	for i, list1i in enumerate(list1):
-		if isinstance(list1i, bool):
-			assert list1i is list2[i]
-		else:
-			assert_allclose(list1i, list2[i], rtol=1e-03)
+    for i, list1i in enumerate(list1):
+        if isinstance(list1i, bool):
+            assert list1i is list2[i]
+        else:
+            assert_allclose(list1i, list2[i], rtol=1e-03)
