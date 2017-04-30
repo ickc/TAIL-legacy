a24f77bf26224bace71dd85102e0d4a6432d27aa
koosterb <kjoosterbaan@gmail.com>
2017-04-30 15:10:09 -0700

    made poly notebook runnable

--- tests/timestream/context.py
+++ tests/timestream/context.py
@@ -2,5 +2,5 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..' + os.sep + '..'))
 
-from tail.timestream.ground_template_filter_array import *
+from tail.timestream.ground_template_filter_array_python import *
 from tail.timestream.poly_filter_array import *
