a071de2425166ad40ebe6d64965aced537bd2aaa
Kolen Cheung <christian.kolen@gmail.com>
2017-04-29 17:09:37 -0700

    tests/helper.py: allow atol to be machine epsilon

--- tests/helper.py
+++ tests/helper.py
@@ -1,10 +1,11 @@
 import numpy as np
 from numpy.testing import assert_allclose
 
+EPSILON = 7./3 - 4./3 - 1
 
 def assertIdenticalList(list1, list2):
     for i, list1i in enumerate(list1):
         if isinstance(list1i, bool):
             assert list1i is list2[i]
         else:
-            assert_allclose(list1i, list2[i], rtol=1e-03)
+            assert_allclose(list1i, list2[i], rtol=1e-03, atol=EPSILON)
