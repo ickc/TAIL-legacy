5373a38afbaa1060cc57ed5f54563b687bf12755
Kolen Cheung <christian.kolen@gmail.com>
2017-05-01 21:25:54 -0700

    make autopep8

--- setup.py
+++ setup.py
@@ -8,20 +8,20 @@ Cython.Compiler.Options.annotate = True
 
 extensions = [
     Extension("*", ["**/*.pyx"],
-        include_dirs = [numpy.get_include()],
-        extra_compile_args=[
-            '-fopenmp',
-            '-Ofast',
-            '-pipe',
-            '-march=native',
-            '-mtune=native',
-            '-std=c++14',
-        ],
+              include_dirs=[numpy.get_include()],
+              extra_compile_args=[
+        '-fopenmp',
+        '-Ofast',
+        '-pipe',
+        '-march=native',
+        '-mtune=native',
+        '-std=c++14',
+    ],
         extra_link_args=['-fopenmp'],
     ),
 ]
 
 setup(
-    name = "Ground Template Filter Array",
-    ext_modules = cythonize(extensions),  # accepts a glob pattern
+    name="Ground Template Filter Array",
+    ext_modules=cythonize(extensions),  # accepts a glob pattern
 )
