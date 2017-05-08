4c9af26c38d779c5a1ff8edea0be00265b8b6e3b
Kolen Cheung <christian.kolen@gmail.com>
2017-05-07 23:47:35 -0700

    test_poly_filter_array.py: add simulate_poly_input

--- tests/timestream/test_poly_filter_array.py
+++ tests/timestream/test_poly_filter_array.py
@@ -5,6 +5,23 @@ import pickle
 from tests.helper import assertIdenticalList
 import sys
 py2 = sys.version_info[0] == 2
+import numpy as np
+
+def simulate_poly_input(nCh, nTime, polyorder):
+    az_time_width = 400
+
+    input_array = (np.random.rand(nCh, nTime) - 0.5) * 0.2
+
+    mask_half_width = 50
+    mask = np.repeat([[(i % az_time_width < mask_half_width
+                        or i % az_time_width > az_time_width - mask_half_width
+                        ) for i in range(nTime)]], nCh, axis=0)
+
+    scan0 = np.arange(171, 10000, 408)
+    scan1 = np.full_like(scan0, 397)
+    scan_list = np.dstack((scan0, scan1))[0]
+
+    return input_array, mask, mask, scan_list, 0, polyorder
 
 
 def test_poly_filter_array():
