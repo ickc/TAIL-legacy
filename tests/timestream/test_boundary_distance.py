d4e4ab0b0f18f360181ecdf10ad4b58512fadee4
Kolen Cheung <christian.kolen@gmail.com>
2017-05-07 18:52:01 -0700

    test_boundary_distance.py: pep8

--- tests/timestream/test_boundary_distance.py
+++ tests/timestream/test_boundary_distance.py
@@ -8,7 +8,7 @@ def circular_mask(n, r):
     mask = np.zeros((n, n), dtype=bool)
     for i in range(n):
         for j in range(n):
-            if (i - n / 2)**2 + (j - n / 2)**2 < r**2:
+            if (i - n / 2) ** 2 + (j - n / 2) ** 2 < r ** 2:
                 mask[i][j] = True
     return mask
 
@@ -20,9 +20,9 @@ def test_boundary_distance():
         [[2, 2], [2, 3], [2, 4], [3, 4], [4, 4], [4, 3], [4, 2], [3, 2]]))
     np.testing.assert_array_equal(box, np.array([2, 2, 4, 4]))
     np.testing.assert_array_equal(apodization, np.array(
-        [[0.,  0.,  0.,  0.,  0.,  0.],
-         [0.,  0.,  0.,  0.,  0.,  0.],
-         [0.,  0.,  0.,  0.,  0.,  0.],
-         [0.,  0.,  0.,  1.,  0.,  0.],
-         [0.,  0.,  0.,  0.,  0.,  0.],
-         [0.,  0.,  0.,  0.,  0.,  0.]]))
+        [[0., 0., 0., 0., 0., 0.],
+         [0., 0., 0., 0., 0., 0.],
+         [0., 0., 0., 0., 0., 0.],
+         [0., 0., 0., 1., 0., 0.],
+         [0., 0., 0., 0., 0., 0.],
+         [0., 0., 0., 0., 0., 0.]]))
